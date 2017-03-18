c764fdebee6a65cdc747ee7291453c361248670f
Chiradeep Vittal <chiradeepv@gmail.com>
2017-03-18 02:09:12 +0000

    Make traffic gen configurable

--- scenarios/traffic_gen/trafficgen.tf
+++ scenarios/traffic_gen/trafficgen.tf
@@ -1,9 +1,9 @@
 module "traffic_gen" {
-  source = "../../config/modules/traffic_gen"
+  source = "../../config/modules/traffic_gen_spotfleet"
 
   base_name                  = "${var.base_name}"
   vpc_id                     = "${module.vpc.vpc_id}"
   key_name                   = "${var.key_name}"
   public_subnet              = "${module.vpc.public_subnets[0]}"
-  traffic_gen_instance_count = 3
+  traffic_gen_instance_count = "${var.num_instances}"
 }
