c4e809e6898b2e22dd59a8a834f67a88a6c5b9e3
Chiradeep Vittal <chiradeepv@gmail.com>
2017-03-18 03:30:56 +0000

    typo fixes

--- scenarios/vpx_single_zone/invoke.tf
+++ scenarios/vpx_single_zone/invoke.tf
@@ -1,4 +1,4 @@
-/* invoke the lambda every 15 minutes */
+/* invoke the lambda every 5 minutes */
 resource "aws_cloudwatch_event_rule" "invoke_lambda_periodic" {
   depends_on          = ["module.autoscale_lambda"]
   name                = "invoke_lambda_periodic"
@@ -7,14 +7,14 @@ resource "aws_cloudwatch_event_rule" "invoke_lambda_periodic" {
 
 resource "aws_cloudwatch_event_target" "invoke_lambda_periodic" {
   rule      = "${aws_cloudwatch_event_rule.invoke_lambda_periodic.name}"
-  target_id = "netscaler_autoscale_lambda"
+  target_id = "netscaler_autoscale_lambda.lambda_name"
   arn       = "${module.autoscale_lambda.lambda_arn}"
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch_to_invoke_lambda" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = "${module.autoscale_lambda.lambda_name}"
+  function_name = "${module.autoscale_lambda.lambda_arn}"
   principal     = "events.amazonaws.com"
   source_arn    = "${aws_cloudwatch_event_rule.invoke_lambda_periodic.arn}"
 }
